d771b889ab4d45c444831a268fd6a321f45e317c
Florian Thiévent <hi@thievent.org>
2022-01-31 23:56:57 +0100

    change buttonProjector to callback

--- global-projectors/buttonProjector.js
+++ global-projectors/buttonProjector.js
@@ -4,17 +4,15 @@ export {buttonProjector}
  * Creates a <button> Element
  * @param text {string}
  * @param type {string[]}
- * @param classList
  * @param callback
  * @returns {HTMLElement} - Returns the anchor Element
  */
-const buttonProjector = (text = "Change this", type= ["button-default"], classList = [], callback=(()=>{}) ) => {
+const buttonProjector = (text = "Change this", type= ["button-default"], callback=(()=>{}) ) => {
 
     const buttonElement = document.createElement('button');
     buttonElement.innerText = text;
     buttonElement.classList.add('button');
     buttonElement.classList.add(...type);
-    classList.forEach( c => buttonElement.classList.add(c));
 
     buttonElement.onclick = _ => callback();
 
