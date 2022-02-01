189706ddbca7616bb3a638b3fd81bba111c2eb71
Florian Thiévent <hi@thievent.org>
2022-02-01 21:18:48 +0100

    removed not anymore used files and code

--- monolog/app.js
+++
@@ -1,8 +0,0 @@
-import {Controller, View} from "./controller.js";
-import {monologService} from "./service/service.js";
-
-const service = monologService();
-const controller = Controller(service);
-
-View(controller, document.getElementById('app'));
-controller.addMonolog();
