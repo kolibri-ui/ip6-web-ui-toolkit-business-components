3a5c204bdabd6b611a373bcb900d9aac1de90fe7
Florian Thiévent <hi@thievent.org>
2022-02-13 15:01:15 +0100

    added all buttons for all kind of monolog

--- demo/monolog/monolog-demo-projector.js
+++ demo/monolog/monolog-demo-projector.js
@@ -8,16 +8,51 @@ const monologDemoProjector = (controller, rootElement) => {
     const monolog = Monolog();
     rootElement.appendChild(monolog.list());
 
-    const pElement = document.createElement("p");
-    pElement.classList.add('demo-text');
-    pElement.textContent = "Show Notifications by pushing the Button below.";
-    rootElement.appendChild(pElement);
+    const pStickyElement = document.createElement("p");
+    pStickyElement.classList.add('demo-text');
+    pStickyElement.textContent = "Show Sticky Notifications by pushing the Button below.";
+    rootElement.appendChild(pStickyElement);
+    pStickyElement.appendChild(document.createElement('br'));
+    pStickyElement.appendChild(document.createElement('br'));
 
-    const successStickButton = buttonProjector("Success Sticky", "button-success", () => {
+    const infoStickyButton = buttonProjector("Info", "button-info", () => {
+        monolog.info({
+            title: "Some Info",
+            message: "And some Text",
+            sticky: true
+        });
+    });
+
+    const successStickButton = buttonProjector("Success", "button-success", () => {
         monolog.success({
             title: "Some Info",
             message: "Just a Test",
             sticky: true
+        });
+    });
+
+    const warningStickyButton = buttonProjector("Warning", "button-warning", () => {
+        monolog.warning({
+            title: "Some Warning",
+            message: "Just a Test",
+            sticky: true
+        });
+    });
+
+    const errorStickyButton = buttonProjector("Error", "button-error", () => {
+        monolog.error({
+            title: "Some Error",
+            message: "Just a Test",
+            sticky: true
+        });
+    });
+
+    const errorStickyAttentionButton = buttonProjector("Error Attention", "button-error", () => {
+        monolog.error({
+            title: "Some Error",
+            message: "Just a Test",
+            sticky: true,
+            attention: true,
         });
     });
 
@@ -32,14 +67,55 @@ const monologDemoProjector = (controller, rootElement) => {
         });
     });
 
+    pStickyElement.appendChild(infoStickyButton);
+    pStickyElement.appendChild(successStickButton);
+    pStickyElement.appendChild(warningStickyButton);
+    pStickyElement.appendChild(errorStickyButton);
+    pStickyElement.appendChild(errorStickyAttentionButton);
+    pStickyElement.appendChild(codeErrorButton);
+    pStickyElement.appendChild(document.createElement('br'));
+    pStickyElement.appendChild(document.createElement('br'));
+
+    const pElement = document.createElement("p");
+    pElement.classList.add('demo-text');
+    pElement.textContent = "Show Non-Sticky Notifications by pushing the Button below.";
+    rootElement.appendChild(pElement);
+    pElement.appendChild(document.createElement('br'));
+    pElement.appendChild(document.createElement('br'));
+
     const infoButton = buttonProjector("Info", "button-info", () => {
         monolog.info({
-            title: "An Info",
+            title: "Some Info",
             message: "And some Text"
         });
     });
 
-    rootElement.appendChild(successStickButton);
-    rootElement.appendChild(codeErrorButton);
-    rootElement.appendChild(infoButton);
+    const successButton = buttonProjector("Success", "button-success", () => {
+        monolog.success({
+            title: "Some Info",
+            message: "Just a Test"
+        });
+    });
+
+    const warningButton = buttonProjector("Warning", "button-warning", () => {
+        monolog.warning({
+            title: "Some Warning",
+            message: "Just a Test"
+        });
+    });
+
+    const errorButton = buttonProjector("Error", "button-error", () => {
+        monolog.error({
+            title: "Some Error",
+            message: "Just a Test"
+        });
+    });
+
+    pElement.appendChild(infoButton);
+    pElement.appendChild(successButton);
+    pElement.appendChild(warningButton);
+    pElement.appendChild(errorButton);
+    pElement.appendChild(codeButton);
+
+
 }
