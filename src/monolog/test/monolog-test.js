cc49d5aa151b2b79fb7f2bb2c10d6ac4fc934d65
Florian Thiévent <florianjeremias@gmail.com>
2022-02-14 22:11:54 +0100

    updated tests

--- src/monolog/test/monolog-test.js
+++ src/monolog/test/monolog-test.js
@@ -1,19 +1,27 @@
 import {TestSuite} from "../../Kolibri/docs/src/kolibri/util/test.js";
-import {Observable} from "../../Kolibri/docs/src/kolibri/observable.js";
+import {Monolog} from "../projector/Monolog.js";
 
 
 const testSuite = TestSuite("monolog");
-const testObservable = Observable(false);
+const monolog     = Monolog();
 
+testSuite.add("monolog-check-list-creation", assert => {
 
-testSuite.add("switch-default-init-test", assert => {
+    const monologList = monolog.list();
+
+    assert.isTrue(monologList !== undefined);
+    assert.isTrue(typeof monologList === 'object');
+    assert.isTrue(monologList.classList.contains("monolog-list"))
+    assert.isTrue(monologList.classList.contains("top"))
+    assert.isTrue(monologList.classList.contains("right"))
+
 });
 
-testSuite.add("monolog-init-test", assert => {
-});
-
-testSuite.add("test-id-creation", assert => {
-});
+testSuite.add("monolog-info-test", assert => {});
+testSuite.add("monolog-success-test", assert => {});
+testSuite.add("monolog-warning-test", assert => {});
+testSuite.add("monolog-error-test", assert => {});
+testSuite.add("monolog-code-error-test", assert => {});
 
 
 testSuite.run();
