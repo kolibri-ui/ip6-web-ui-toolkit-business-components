f3cebd3408d8d896539622b15bea04a8f042c4e1
Alexander <alexander.eser@students.fhnw.ch>
2022-03-15 12:19:53 +0100

    Added test cases for stacking monologues and placing in the correct stack lists.

--- src/monolog/test/monolog-test.js
+++ src/monolog/test/monolog-test.js
@@ -26,23 +26,47 @@ testSuite.add("monolog-check-list-creation", assert => {
 
 });
 
-testSuite.add("stack-list-info-test", assert => {
+/*** Check if there are 4 stackLists created ***/
+testSuite.add("stack-list-number-test", assert => {
     const monologList = monologListElement.list();
     const stackLists  = monologList.children;
     assert.isTrue(stackLists.length === 4);
+});
 
-    options.type   = 'info';
-    options.sticky = true;
+/*** Stack 3 info monologues and check if they are placed in the correct stackList ***/
+testSuite.add("stack-list-info-test", assert => {
+    const monologList = monologListElement.list();
 
     monologListElement.info(options);
     monologListElement.info(options);
     monologListElement.info(options);
 
-    const stackListInfo       = monologList.querySelector(".stack-list-info");
-    const monologInfoElements = stackListInfo.querySelectorAll(".monolog.info");
+    const monologInfoElements = monologList.querySelectorAll(".monolog.info");
     assert.isTrue(monologInfoElements.length === 3);
 
+    const stackListInfo = monologInfoElements[0].parentElement;
+    assert.isTrue(stackListInfo.classList.contains("stack-list-info"));
+
+    const stackListInfoCounter     = stackListInfo.querySelector(".info-number");
+    stackListInfoCounter.innerText = monologInfoElements.length.toString();
+    assert.isTrue(stackListInfoCounter.innerText === '3');
 });
+
+/*** Stack 2 success monologues and check if they are placed in the correct stackList ***/
+testSuite.add("stack-list-success-test", assert => {
+    const monologList = monologListElement.list();
+
+    monologListElement.success(options);
+    monologListElement.success(options);
+
+    const monologSuccessElements = monologList.querySelectorAll(".monolog.success");
+    assert.isTrue(monologSuccessElements.length === 2);
+
+    const stackListInfo = monologSuccessElements[0].parentElement;
+    assert.isTrue(stackListInfo.classList.contains("stack-list-success"));
+
+});
+
 
 /*** MonologList Tests End ***/
 
