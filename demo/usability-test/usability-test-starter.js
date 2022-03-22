e9a04d8a5b033ee2881c2dd4b8f95a00452495a2
Alexander <alexander.eser@students.fhnw.ch>
2022-03-22 12:13:02 +0100

    Added interactive monologues to usability testing script.

--- demo/usability-test/usability-test-starter.js
+++ demo/usability-test/usability-test-starter.js
@@ -1,5 +1,6 @@
 import { DemoSwitch } from "../switch/DemoSwitch.js";
 import { Observable } from "../../src/Kolibri/docs/src/kolibri/observable.js";
+import {MonologList} from "../../src/monolog/projector/MonologList.js";
 
 
 /**
@@ -38,3 +39,178 @@ document.getElementById("slim-switch-shadow").appendChild(slimshadowOnSwitch);
 /**
  * Monolog Test
  */
+
+const monologInfo = MonologList();
+monologInfo.list().classList.remove("right", "top");
+monologInfo.list().classList.add("center");
+
+monologInfo.info({
+    title  : "Info",
+    message: "Meeting - 22.03.2022. Ihr Meeting startet in 10 Minuten.",
+    sticky : true,
+});
+document.getElementById("info-monolog").appendChild(monologInfo.list());
+
+const monologSuccess = MonologList();
+monologSuccess.list().classList.remove("right", "top");
+monologSuccess.list().classList.add("center");
+
+monologSuccess.success({
+  title  : "Success",
+  message: "Das Formular wurde erfolgreich gesendedt. Ihre Anfrage wurde bestätigt.",
+  sticky : true,
+});
+document.getElementById("success-monolog").appendChild(monologSuccess.list());
+
+const monologWarning = MonologList();
+monologWarning.list().classList.remove("right", "top");
+monologWarning.list().classList.add("center");
+
+monologWarning.warning({
+  title  : "Warning",
+  message: "Es gibt ein Problem mit Ihrer Netzwerkverbindung.",
+  sticky : true,
+});
+document.getElementById("warning-monolog").appendChild(monologWarning.list());
+
+const monologError = MonologList();
+monologError.list().classList.remove("right", "top");
+monologError.list().classList.add("center");
+
+monologError.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten beim Übermitteln Ihrer Daten.",
+  sticky : true,
+});
+document.getElementById("error-monolog").appendChild(monologError.list());
+
+const monologCodeError = MonologList();
+monologCodeError.list().classList.remove("right", "top");
+monologCodeError.list().classList.add("center");
+
+monologCodeError.error({
+  title  : "Code Error",
+  message: "Konsultieren Sie einen Programmierer.",
+  sticky : true,
+  codeError: 'Exception in thread "main" java.lang.NullPointerException at com.example.myproject.Book.getTitle(Book.java:16) at ...'
+});
+document.getElementById("code-error-monolog").appendChild(monologCodeError.list());
+
+
+const monologCompare = MonologList();
+monologCompare.list().classList.remove("right", "top");
+monologCompare.list().classList.add("center");
+monologCompare.info({
+  title  : "Info",
+  message: "Meeting - 22.03.2022. Ihr Meeting startet in 10 Minuten.",
+  sticky : true,
+});
+monologCompare.success({
+  title  : "Success",
+  message: "Das Formular wurde erfolgreich gesendedt. Ihre Anfrage wurde bestätigt.",
+  sticky : true,
+});
+monologCompare.warning({
+  title  : "Warning",
+  message: "Es gibt ein Problem mit Ihrer Netzwerkverbindung.",
+  sticky : true,
+});
+monologCompare.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten beim Übermitteln Ihrer Daten.",
+  sticky : true,
+});
+monologCompare.error({
+  title  : "Code Error",
+  message: "Konsultieren Sie einen Programmierer.",
+  sticky : true,
+  codeError: 'Exception in thread "main" java.lang.NullPointerException at com.example.myproject.Book.getTitle(Book.java:16) at ...'
+});
+document.getElementById("monolog-compare").appendChild(monologCompare.list());
+
+const monologError2 = MonologList();
+monologError2.list().classList.remove("right", "top");
+monologError2.list().classList.add("center");
+monologError2.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten beim Übermitteln Ihrer Daten.",
+  sticky : true,
+});
+document.getElementById("error-monolog-2").appendChild(monologError2.list());
+
+
+const monologErrorStack = MonologList();
+monologErrorStack.list().classList.remove("right", "top");
+monologErrorStack.list().classList.add("center");
+monologErrorStack.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten.",
+  sticky : true,
+});
+monologErrorStack.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten.",
+  sticky : true,
+});
+monologErrorStack.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten.",
+  sticky : true,
+});
+monologErrorStack.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten.",
+  sticky : true,
+});
+document.getElementById("stacked-error-monolog").appendChild(monologErrorStack.list());
+
+const monologErrorStack2 = MonologList();
+monologErrorStack2.list().classList.remove("right", "top");
+monologErrorStack2.list().classList.add("center");
+monologErrorStack2.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten.",
+  sticky : true,
+});
+monologErrorStack2.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten.",
+  sticky : true,
+});
+monologErrorStack2.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten.",
+  sticky : true,
+});
+monologErrorStack2.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten.",
+  sticky : true,
+});
+document.getElementById("stacked-error-monolog-2").appendChild(monologErrorStack2.list());
+
+const monologErrorStack3 = MonologList();
+monologErrorStack3.list().classList.remove("right", "top");
+monologErrorStack3.list().classList.add("center");
+monologErrorStack3.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten.",
+  sticky : true,
+});
+monologErrorStack3.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten.",
+  sticky : true,
+});
+monologErrorStack3.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten.",
+  sticky : true,
+});
+monologErrorStack3.error({
+  title  : "Error",
+  message: "Ein Fehler ist aufgetreten.",
+  sticky : true,
+});
+document.getElementById("stacked-error-monolog-3").appendChild(monologErrorStack3.list());
+
